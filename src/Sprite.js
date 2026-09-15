0b455ef8f6db029aec864e26b036f806f24da288
Mattias Mellin <mm225vh@student.lnu.se>
2026-09-15 16:16:46 +0200

    Add function to set sprite size, change private methods to public

--- src/Sprite.js
+++ src/Sprite.js
@@ -4,45 +4,61 @@ export class Sprite {
   #xPos
   #yPos
   #loaded
+  #width
+  #height
 
-  constructor (src, xPos = 0, yPos = 0) {
+  constructor (src, xPos = 0, yPos = 0, width, height) {
     this.#createImage()
     this.setPosition(xPos, yPos)
+    this.#setSpriteSize(width, height)
 
     this.#loaded = new Promise((resolve, reject) => {
       this.#image.onload = resolve
       this.#image.onerror = reject
-      this.#setSrc(src)
+      this.setImageSource(src)
     })
+  }
+
+  #setSpriteSize (width, height) {
+    this.#width = width
+    this.#height = height
   }
 
   #createImage () {
     this.#image = new Image()
   }
 
-  #setSrc (src) {
+  setImageSource (src) {
     this.#image.src = src
   }
 
   setPosition (xPos, yPos) {
-    this.#positionX = xPos
-    this.#positionY = yPos
+    this.positionX = xPos
+    this.positionY = yPos
   }
 
   get positionX () {
     return this.#xPos
   }
 
-  set #positionX (xPos) {
-    this.#xPos = xPos
+  set positionX (xPos) {
+    if (typeof xPos !== 'number') {
+      throw new TypeError('xPos must be a number')
+    } else {
+      this.#xPos = xPos
+    }
   }
 
   get positionY () {
     return this.#yPos
   }
 
-  set #positionY (yPos) {
-    this.#yPos = yPos
+  set positionY (yPos) {
+    if (typeof yPos !== 'number') {
+      throw new TypeError('yPos must be a number')
+    } else {
+      this.#yPos = yPos
+    }
   }
 
   get image() {
@@ -51,6 +67,14 @@ export class Sprite {
 
   async waitForLoad () {
     await this.#loaded
+  }
+
+  get width () {
+    return this.#width
+  }
+
+  get height () {
+    return this.#height
   }
 }
 
