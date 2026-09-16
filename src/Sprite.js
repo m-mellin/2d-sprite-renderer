2513331cb0c5cb620438f3cf57d4f9ca585d4eb8
Mattias Mellin <mm225vh@student.lnu.se>
2026-09-16 18:04:22 +0200

    Fix issue with incorrect use of setter

--- src/Sprite.js
+++ src/Sprite.js
@@ -9,8 +9,8 @@ export class Sprite {
   #region
 
   constructor (src, xPos = 0, yPos = 0, width, height, region = null) {
-    this.#xPos = this.positionX(xPos)
-    this.#yPos = this.positionY(yPos)
+    this.positionX = xPos
+    this.positionY = yPos
     this.#width = width
     this.#height = height
     this.setImageSource(src)
