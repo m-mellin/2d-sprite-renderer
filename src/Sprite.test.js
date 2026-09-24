b3f71334f6466c67db50e77c92e43f42123263e5
Mattias Mellin <mm225vh@student.lnu.se>
2026-09-24 14:47:00 +0200

    Add jsdom dependency, add first unit test for Sprite class

---
+++ src/Sprite.test.js
@@ -0,0 +1,94 @@
+import { describe, it, expect } from 'vitest'
+import { Sprite } from './Sprite.js'
+import { SpriteRegion } from './SpriteRegion.js'
+
+const src = '../src/mario.bmp'
+
+describe('Sprite', () => {
+  it('creates a Sprite with default values', () => {
+    const sprite = new Sprite(src)
+
+    expect(sprite.image).toBeInstanceOf(HTMLImageElement)
+    expect(sprite.width).toBeUndefined()
+    expect(sprite.height).toBeUndefined()
+    expect(sprite.x).toBe(0)
+    expect(sprite.y).toBe(0)
+  })
+
+  it('creates a Sprite with specified x and y coordinates', () => {
+    const sprite = new Sprite(src, {x: 5, y: 5})
+
+    expect(sprite.x).toBe(5)
+    expect(sprite.y).toBe(5)
+  })
+
+  it('creates a Sprite with specified width and height', () => {
+    const sprite = new Sprite(src, {width: 5, height: 5})
+
+    expect(sprite.width).toBe(5)
+    expect(sprite.height).toBe(5)
+  })
+
+  it('changes x and y value after creating sprite', () => {
+    const sprite = new Sprite(src, {x: 5, y: 5})
+
+    sprite.x = 10
+    sprite.y = 15
+    expect(sprite.x).toBe(10)
+    expect(sprite.y).toBe(15)
+  })
+
+  it('get x returns correct coordinate', () => {
+    const sprite = new Sprite(src, {x: 5})
+
+    expect(sprite.x).toBe(5)
+  })
+
+  it('get y returns correct coordinate', () => {
+    const sprite = new Sprite(src, {y: 5})
+
+    expect(sprite.y).toBe(5)
+  })
+
+  it('get width returns correct width', () => {
+    const sprite = new Sprite(src, {width: 5})
+
+    expect(sprite.width).toBe(5)
+  })
+
+  it('get height returns correct height', () => {
+    const sprite = new Sprite(src, {height: 5})
+
+    expect(sprite.height).toBe(5)
+  })
+
+  it('get region returns correct region', () => {
+    const region = new SpriteRegion(25, 25, 50, 50)
+    const sprite = new Sprite(src, { region })
+
+    expect(sprite.region).toBe(region)
+  })
+
+  it('set region sets new region', () => {
+    const region = new SpriteRegion(25, 25, 50, 50)
+    const sprite = new Sprite(src, { region })
+
+    const newRegion = new SpriteRegion(2, 2, 30, 30)
+    sprite.region = newRegion
+
+    expect(sprite.region).toBe(newRegion)
+  })
+
+  it('isLoaded returns false while sprite isn\'t loaded', () => {
+    const sprite = new Sprite(src)
+
+    expect(sprite.isLoaded).toBe(false)
+  })
+
+  it('waitForLoad() returns promise', () => {
+    const sprite = new Sprite(src)
+
+    expect(sprite.waitForLoad()).toBeInstanceOf(Promise)
+  })
+
+})
