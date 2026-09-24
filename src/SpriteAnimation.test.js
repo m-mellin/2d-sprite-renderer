ba4a8ece25e14139b9cc4ad77eb69df2ed2af13b
Mattias Mellin <mm225vh@student.lnu.se>
2026-09-24 18:39:09 +0200

    Add update tests to SpriteAnimation unit test

--- src/SpriteAnimation.test.js
+++ src/SpriteAnimation.test.js
@@ -1,5 +1,4 @@
 import { describe, it, expect } from 'vitest'
-import { Sprite } from './Sprite.js'
 import { SpriteRegion } from './SpriteRegion.js'
 import { SpriteAnimation } from './SpriteAnimation.js'
 
@@ -16,6 +15,41 @@ describe('SpriteAnimation', () => {
 
       expect(animation.region).toBe(frame1)
     })
+
+  describe('Update', () => {
+    it('does not change frame while elapsed time is less than frame duration', () => {
+      const animation = new SpriteAnimation([frame1, frame2], 100)
+
+      animation.update(75)
+      
+      expect(animation.region).toBe(frame1)
+    })
+
+    it('changes to next frame when elapsed time is equal to frame duration', () => {
+      const animation = new SpriteAnimation([frame1, frame2], 100)
+
+      animation.update(100)
+
+      expect(animation.region).toBe(frame2)
+    })
+
+    it('changes frame when elapsed time is greater than frame duration', () => {
+      const animation = new SpriteAnimation([frame1, frame2], 100)
+
+      animation.update(125)
+
+      expect(animation.region).toBe(frame2)
+    })
+
+    it('returns to first frame after last frame', () => {
+      const animation = new SpriteAnimation([frame1, frame2], 100)
+
+      animation.update(100)
+      animation.update(100)
+
+      expect(animation.region).toBe(frame1)
+    })
+  })
   })
 
 })
