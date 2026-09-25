a289af6e82c615be665235da8595a1874b72b84f
Mattias Mellin <mm225vh@student.lnu.se>
2026-09-25 14:27:11 +0200

    Fix issue with character not idleing in correct direction

--- test-app/app.js
+++ test-app/app.js
@@ -14,7 +14,10 @@ body.appendChild(canvas)
 
 const scale = 4
 
-const character = new Sprite('assets/orc1_walk_full.png', {x: 10, y: 10, width: 64 * scale, height: 64 * scale})
+const walkSrc = 'assets/orc1_walk_full.png'
+const idleSrc = 'assets/orc1_idle_full.png'
+
+const character = new Sprite(idleSrc, {x: 10, y: 10, width: 64 * scale, height: 64 * scale})
 
 renderer.add(character)
 renderer.render()
@@ -55,33 +58,35 @@ const walkRightRegion = [
   new SpriteRegion({x: 320, y: 192, width: 64, height: 64})
 ]
 
-const idleUpRegion = [
+
+const idleDownRegion = [
   new SpriteRegion({x: 0, y: 0, width: 64, height: 64}),
   new SpriteRegion({x: 64, y: 0, width: 64, height: 64}),
   new SpriteRegion({x: 128, y: 0, width: 64, height: 64}),
   new SpriteRegion({x: 192, y: 0, width: 64, height: 64}),
 ]
 
-const idleDownRegion = [
+const idleUpRegion = [
   new SpriteRegion({x: 0, y: 64, width: 64, height: 64}),
   new SpriteRegion({x: 64, y: 64, width: 64, height: 64}),
   new SpriteRegion({x: 128, y: 64, width: 64, height: 64}),
   new SpriteRegion({x: 192, y: 64, width: 64, height: 64}),
 ]
 
-const idleRightRegion = [
+const idleLeftRegion = [
   new SpriteRegion({x: 0, y: 128, width: 64, height: 64}),
   new SpriteRegion({x: 64, y: 128, width: 64, height: 64}),
   new SpriteRegion({x: 128, y: 128, width: 64, height: 64}),
   new SpriteRegion({x: 192, y: 128, width: 64, height: 64}),
 ]
 
-const idleLeftRegion = [
+const idleRightRegion = [
   new SpriteRegion({x: 0, y: 192, width: 64, height: 64}),
   new SpriteRegion({x: 64, y: 192, width: 64, height: 64}),
   new SpriteRegion({x: 128, y: 192, width: 64, height: 64}),
   new SpriteRegion({x: 192, y: 192, width: 64, height: 64}),
 ]
+
 
 const walkingDown = new SpriteAnimation(walkDownRegion, 100)
 const walkingUp = new SpriteAnimation(walkUpRegion, 100)
@@ -97,21 +102,25 @@ let currentAnimation = idleDown
 
 document.addEventListener('keydown', (event) => {
   if (event.key === 'ArrowDown') {
+    character.assignImageAsset(walkSrc)
     currentAnimation = walkingDown
     character.y += 5
   }
 
   if (event.key === 'ArrowUp') {
+    character.assignImageAsset(walkSrc)
     currentAnimation = walkingUp
     character.y -= 5
   }
 
   if (event.key === 'ArrowLeft') {
+    character.assignImageAsset(walkSrc)
     currentAnimation = walkingLeft
     character.x -= 5
   }
 
   if (event.key === 'ArrowRight') {
+    character.assignImageAsset(walkSrc)
     currentAnimation = walkingRight
     character.x += 5
   }
@@ -119,18 +128,22 @@ document.addEventListener('keydown', (event) => {
 
 document.addEventListener('keyup', (event) => {
   if (event.key === 'ArrowDown') {
+    character.assignImageAsset(idleSrc)
     currentAnimation = idleDown
   }
 
   if (event.key === 'ArrowUp') {
+    character.assignImageAsset(idleSrc)
     currentAnimation = idleUp
   }
 
   if (event.key === 'ArrowLeft') {
+    character.assignImageAsset(idleSrc)
     currentAnimation = idleLeft
   }
 
   if (event.key === 'ArrowRight') {
+    character.assignImageAsset(idleSrc)
     currentAnimation = idleRight
   }
 })
